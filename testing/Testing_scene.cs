f7cad7393ecf113da2ab938d2f06cc3c57534731
= <satyameshmaliaass@gmail.com>
2022-11-02 12:45:23 +0530

    some1

--- testing/Testing_scene.cs
+++ testing/Testing_scene.cs
@@ -10,12 +10,14 @@ public class Testing_scene : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
 		
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		
+		
+	}
 }
